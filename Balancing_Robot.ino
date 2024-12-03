8ea7d63196408328416a8cff46878048447201b8
Zimam <mzimam.ath@gmail.com>
2024-12-03 15:57:22 +0700

    Refactory and Tuning Parameters

--- Balancing_Robot.ino
+++ Balancing_Robot.ino
@@ -6,9 +6,14 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 
-bool robotActive = false;
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+bool ACTIVATE = false;
+bool RUN_MOTOR = true;
 #define DEBUG 0
-#define SEND_DATA 0
+#define SEND_DATA 1
 
 #define BUZZER_PIN 13
 #define INTERRUPT_PIN 19
@@ -43,18 +48,6 @@ uint8_t echoPin[4] = {27, 29, 31, 33};
 #define READ_BUTTON2 !digitalRead(PB2_PIN)
 #define MIN_DISTANCE 20 // Minimum distance to an obstacle in cm
 
-// Library Initializing
-MPU6050 mpu;
-Encoder encoderMotorL(encoderAMotorL, encoderBMotorL);
-Encoder encoderMotorR(encoderAMotorR, encoderBMotorR);
-Ultrasonic US1(trigPin[0], echoPin[0]);
-Ultrasonic US2(trigPin[1], echoPin[1]);
-Ultrasonic US3(trigPin[2], echoPin[2]);
-Ultrasonic US4(trigPin[3], echoPin[3]);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-JsonDocument set;
-JsonDocument get;
-
 // LCD Initializing message
 char initMsg[16] = {'I','n','i','t','i','a','l','i','z','i','n','g','.','.','.','.'};
 
@@ -65,39 +58,37 @@ uint8_t devStatus;      // return status after each device operation (0 = succes
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64];
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         Quaternion container
-VectorInt16 aa;         // [x, y, z]            Accel sensor measurements
-VectorInt16 gy;         // [x, y, z]            Gyro sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            Gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            World-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            Gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+VectorInt16 gy;			    // [x, y, z]            Gyro sensor measurements
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+volatile bool mpuInterrupt = false;
 void dmpDataReady() {
     mpuInterrupt = true;
 }
 
 // PID Variables
-double KP = 50; // 60
-double KI = 20; // 60
-double KD = 0;  // 2
+double KP = 26;
+double KI = 0;
+double KD = 0.5;
+double KPy = 0.7;
+double KIy = 0.4;
+double KDy = 0.01;
+double pidRollOut = 0, pidYawOut = 0;
+double speedLeft = 0, speedRight = 0;
+double speedFactor = 0.75;
 
-const double maxSpeed = 255, minSpeed = -255;
-const double setpointPitch = 0;
-double pitchAngle = 0;
-double pidMotorL, pidMotorR;
-double speedGainL = 0.7, speedGainR = 0.6;
+// IMU Variables
+const double maxPID = 255, minPID = -255;
+double setpointRoll = -1.5, setpointYaw = 0.01; // Target desired angle
+double prevRoll = 0, roll = 0, yaw = 0; 		// Actual angle
 double currentSpeedL = 0, currentSpeedR = 0;
+double motorAdjust = 0;
 
-PID pidMotorLeft(&pitchAngle, &pidMotorL, &setpointPitch, KP, KI, KD, DIRECT);
-PID pidMotorRight(&pitchAngle, &pidMotorR, &setpointPitch, KP, KI, KD, DIRECT);
+// Low-pass filter variables
+float filteredRoll = 0.0;
+float alpha = 0.8;
 
 // Encoder variables
 const double wheelCircumference = 0.2042; // meter
@@ -111,13 +102,29 @@ double actualSpeedLeft, actualSpeedRight;
 
 // Ultrasonic variables
 int readUS1, readUS2, readUS3, readUS4;
+int obstacleSpeed = 0, obstacleThreshold = 0;
 
 // GUI last message sent time
 unsigned long lastSendTime = 0;
 
+// Library Initializing
+MPU6050 mpu;
+PID pidRoll(&roll, &pidRollOut, &setpointRoll, KP, KI, KD, DIRECT);
+PID pidYaw(&yaw, &pidYawOut, &setpointYaw, KPy, KIy, KDy, DIRECT);
+Encoder encoderMotorL(encoderAMotorL, encoderBMotorL);
+Encoder encoderMotorR(encoderAMotorR, encoderBMotorR);
+Ultrasonic US1(trigPin[0], echoPin[0]);
+Ultrasonic US2(trigPin[1], echoPin[1]);
+Ultrasonic US3(trigPin[2], echoPin[2]);
+Ultrasonic US4(trigPin[3], echoPin[3]);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+JsonDocument set;
+JsonDocument get;
+
 void setup() {
   Serial.begin(115200);
   Serial2.begin(115200);
+  Serial2.setTimeout(10);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
@@ -149,16 +156,13 @@ void setup() {
   }
 
   // initialize device
-  Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
   pinMode(INTERRUPT_PIN, INPUT);
 
   // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  // Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
   // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
   mpu.setXGyroOffset(29);
@@ -166,19 +170,10 @@ void setup() {
   mpu.setZGyroOffset(-3);
   mpu.setZAccelOffset(1161);
 
-  // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    mpu.PrintActiveOffsets();
-
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
     mpu.setDMPEnabled(true);
 
     // enable ESP32 interrupt detection
-    Serial.println(F("Enabling interrupt detection..."));
     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
     mpuIntStatus = mpu.getIntStatus();
 
@@ -189,13 +184,13 @@ void setup() {
     // get expected DMP packet size for later comparison
     packetSize = mpu.dmpGetFIFOPacketSize();
     
-    pidMotorLeft.SetOutputLimits(minSpeed, maxSpeed);
-    pidMotorLeft.SetMode(AUTOMATIC);
-    pidMotorLeft.SetSampleTime(10);
+    pidRoll.SetOutputLimits(minPID, maxPID);
+    pidRoll.SetMode(AUTOMATIC);
+    pidRoll.SetSampleTime(10);
 
-    pidMotorRight.SetOutputLimits(minSpeed, maxSpeed);
-    pidMotorRight.SetMode(AUTOMATIC);
-    pidMotorRight.SetSampleTime(10);
+    pidYaw.SetOutputLimits(minPID, maxPID);
+    pidYaw.SetMode(AUTOMATIC);
+    pidYaw.SetSampleTime(10);
     lcd.clear();
   } else {
     lcd.clear();
@@ -214,24 +209,20 @@ void setup() {
 }
 
 void loop() {
-  if (robotActive) runRobot();
-
-  if(READ_BUTTON1 && !robotActive){
-    robotActive = true;
+  if(READ_BUTTON1 && !ACTIVATE){
+    ACTIVATE = true;
+    RUN_MOTOR = true;
     BuzzON Led2ON
     delay(200);
     BuzzOFF
-  } else if (READ_BUTTON2 && robotActive) {
-    robotActive = false;
+  } else if (READ_BUTTON2 && ACTIVATE) {
+    ACTIVATE = false;
+    RUN_MOTOR = false;
     BuzzOFF Led1OFF Led2OFF
-    controlMotors(0, 0, 0);
+    controlMotors(0, 0);
   }
 
-  // Read Ultrasonic
-  readUS1 = US1.read();
-  readUS2 = US2.read();
-  readUS3 = US3.read();
-  readUS4 = US4.read();
+  if (ACTIVATE) runRobot();
 
   // Send data to GUI each 100 ms
   if (SEND_DATA && !DEBUG && millis() - lastSendTime >= 100) {
@@ -242,13 +233,15 @@ void loop() {
     get["kp"] = KP;
     get["ki"] = KI;
     get["kd"] = KD;
-    get["pid"] = pidMotorL;
-    get["error"] = setpointPitch - pidMotorL;
+    get["pid"] = pidRollOut;
+    get["setpoint"] = setpointRoll;
+    get["speedfactor"] = speedFactor;
+    get["error"] = setpointRoll - pidRollOut;
     get["distance"] = (lastPositionLeft + lastPositionRight) / 2; // Average of distance motor left and right (m)
-    get["usfl"] = (readUS3 <= 20) ? 1 : 0; // If there is obstacle (in distance less or equal to 20) will return true
-    get["usfr"] = (readUS4 <= 20) ? 1 : 0;
-    get["usbl"] = (readUS1 <= 20) ? 1 : 0;
-    get["usbr"] = (readUS2 <= 20) ? 1 : 0;
+    get["usfl"] = readUS3;
+    get["usfr"] = readUS4;
+    get["usbl"] = readUS1;
+    get["usbr"] = readUS2;
     serializeJson(get, Serial2);
     Serial2.println();
     lastSendTime = millis();
@@ -256,22 +249,32 @@ void loop() {
 
   // Read message from GUI
   if (Serial2.available() > 0) {
-    String data = Serial2.readStringUntil('\n');
+    String data = Serial2.readStringUntil("\n");
     deserializeJson(set, data);
-    KP = set["kp"];
-    KI = set["ki"];
-    KD = set["kd"];
-    pidMotorLeft.SetTunings(KP, KI, KD);
-    pidMotorRight.SetTunings(KP, KI, KD);
+    if (set["kp"]) {
+      KP = set["kp"];
+      KI = set["ki"];
+      KD = set["kd"];
+      setpointRoll = set["setpoint"];
+      // obstacleSpeed = set["threshold"];
+      // speedFactor = set["speedfactor"];
+      pidRoll.SetTunings(KP, KI, KD);
+    } else {
+      unsigned char direction = set["direction"];
+    }
   }
 
-  // Read message from Serial0
   if (Serial.available() > 0) {
-    String message = Serial.readStringUntil('\n');
-    message.trim();
-    String command = message.substring(0, message.indexOf(':'));
-    String value = message.substring(message.indexOf(':') + 1);
-    runCommand(command, value);
+    String data = Serial2.readStringUntil("\n");
+    int limit = data.indexOf(':');
+    String command = data.substring(0, limit);
+    String value = data.substring(limit + 1);
+    if (command == "KP") KP = value.toDouble();
+    if (command == "KI") KI = value.toDouble();
+    if (command == "KD") KD = value.toDouble();
+    if (command == "SP") setpointRoll = value.toDouble();
+    pidRoll.SetTunings(KP, KI, KD);
+    Serial.println(value.toDouble());
   }
 
   // Debugging
@@ -279,8 +282,43 @@ void loop() {
 }
 
 void runRobot() {
-  // Check if DMP is ready before proceeding
   if (!dmpReady) return;
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    mpu.resetFIFO();
+  } else if (mpuIntStatus & 0x02) {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    mpu.getFIFOBytes(fifoBuffer, packetSize); // read a packet from FIFO
+    fifoCount -= packetSize;
+    mpu.dmpGetQuaternion(&q, fifoBuffer); //get value for q
+    mpu.dmpGetGravity(&gravity, &q); //get value for gravity
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity); //get value for ypr
+    mpu.dmpGetGyro(&gy, fifoBuffer); // get gyro rate
+
+    yaw = abs(gy.z / 131.0); // Get yaw gyro rate
+    roll = alpha * roll + (1 - alpha) * actRoll;
+
+    pidRoll.Compute();
+
+    speedLeft = constrain(pidRollOut + pidYawOut + motorAdjust + obstacleSpeed, -255, 255);
+    speedRight = constrain(pidRollOut - pidYawOut - motorAdjust + obstacleSpeed, -255, 255);
+    if (RUN_MOTOR) controlMotors(speedLeft, speedRight);
+  }
+
+  // Read Ultrasonic
+  readUS1 = US1.read();
+  readUS2 = US2.read();
+  readUS3 = US3.read();
+  readUS4 = US4.read();
+
+  if (readUS3 || readUS4) { // Obstacle in front
+    obstacleSpeed = -100;
+  } else if(readUS1 || readUS2) { // Obstacle in back
+    obstacleSpeed = 100;
+  }
 
   // Read Encoder
   unsigned long currentTime = millis();
@@ -304,121 +342,49 @@ void runRobot() {
   actualSpeedRight = (pulsePerSecRight / pulsePerRevolution) * wheelCircumference;
   lastPositionLeft = (actualPulseLeft / pulsePerRevolution) * wheelCircumference;
   lastPositionRight = (actualPulseRight / pulsePerRevolution) * wheelCircumference;
-
-  pidMotorLeft.Compute();
-  pidMotorRight.Compute();
-  controlMotors(pitchAngle, pidMotorL, pidMotorR);
-
-  // Reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-  fifoCount = mpu.getFIFOCount();
-
-  // Check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-      mpu.resetFIFO();
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    fifoCount -= packetSize;
-
-    // Get current orientation from MPU6050
-    mpu.dmpGetQuaternion(&q, fifoBuffer); //get value for q
-    mpu.dmpGetGravity(&gravity, &q); //get value for gravity
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity); //get value for ypr
-    pitchAngle = ypr[1] * 180/M_PI;
-  }
 }
 
-void runCommand(String command, String value) {
-  Serial.print("command ");
-  Serial.print(command);
-  Serial.print("\t");
-  Serial.print("value ");
-  Serial.println(value);
-  if (command == "KP") {
-    KP = value.toDouble();
-  } else if (command == "KI") {
-    KI = value.toDouble();
-  } else if (command == "KD") {
-    KD = value.toDouble();
-  } else if (command == "A") {
-    if (value == "F") {
-      // Forward
-    } else if (value == "B") {
-      // Backward
-    } else if (value == "R") {
-      // Right
-    } else if (value == "L") {
-      // Left
-    } else if (value == "G") {
-      // Grab
-    } else if (value == "P") {
-      // Put
-    }
-  }
-
-  if (command == "KP" || command == "KI" || command == "KD") {
-    Serial.print("KP: ");Serial.print(KP);Serial.print("\t KI: ");Serial.print(KI);Serial.print("\t KD: ");Serial.println(KD);
-    pidMotorLeft.SetTunings(KP, KI, KD);
-    pidMotorRight.SetTunings(KP, KI, KD);
-  }
-}
-
-void controlMotors(double angle, double speedL, double speedR) {
-  if (angle < -50 || angle > 50.0) { // pitch higher than 50 or lower than -50 mean robot is falling down
-    speedL = 0;
-    speedR = 0;
-  } else {
-    speedGainL = 0.7;
-    speedGainR = 0.7;
-  }
-
+void controlMotors(double speedL, double speedR) {
   if (speedL == currentSpeedL && speedR == currentSpeedR) return;
 
   if (speedL > 0) { // forward
-    digitalWrite(motorLPin1, HIGH);
-    digitalWrite(motorLPin2, LOW);
-  } else if (speedL < 0) { // reverse
-    speedL = speedL * -1;
     digitalWrite(motorLPin1, LOW);
     digitalWrite(motorLPin2, HIGH);
+  } else if (speedL < 0) { // reverse
+    speedL = speedL * -1;
+    digitalWrite(motorLPin1, HIGH);
+    digitalWrite(motorLPin2, LOW);
   } else {
     digitalWrite(motorLPin1, LOW);
     digitalWrite(motorLPin2, LOW);
   }
 
   if (speedR > 0) { // forward
-    digitalWrite(motorRPin1, LOW);
-    digitalWrite(motorRPin2, HIGH);
-  } else if (speedR < 0) { // reverse
-    speedR = speedR * -1;
     digitalWrite(motorRPin1, HIGH);
     digitalWrite(motorRPin2, LOW);
+  } else if (speedR < 0) { // reverse
+    speedR = speedR * -1;
+    digitalWrite(motorRPin1, LOW);
+    digitalWrite(motorRPin2, HIGH);
   } else {
     digitalWrite(motorRPin1, LOW);
     digitalWrite(motorRPin2, LOW);
   }
 
-  analogWrite(motorLPWM, speedGainL * speedL);
-  analogWrite(motorRPWM, speedGainR * speedR);
+  analogWrite(motorLPWM, speedFactor * speedL);
+  analogWrite(motorRPWM, speedFactor * speedR);
 
   currentSpeedL = speedL;
   currentSpeedR = speedR;
 }
 
 void debug() {
-  Serial.print("Pitch: ");
-  Serial.print(pitchAngle);
+  Serial.print("Roll: ");
+  Serial.print(roll);
   Serial.print("\t pidMotorL: ");
-  Serial.print(pidMotorL);
+  Serial.print(speedLeft);
   Serial.print("\t pidMotorR: ");
-  Serial.print(pidMotorR);
+  Serial.print(speedRight);
   Serial.print("\t pulseLeft: ");
   Serial.print(pulseLeft);
   Serial.print("\t pulseRight: ");
